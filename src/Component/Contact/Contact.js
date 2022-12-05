9cdbe7741f616a4c63d865e718e3db5ccef5d593
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-12-05 18:50:25 +0530

    v - 24 Contact send button validations added

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -3,25 +3,33 @@ import './Contact.css';
 
 const Contact = () => {
     const [data, setData] = useState({
-        fullname : "",
+        fullname: "",
         phone: "",
         email: "",
         subject: "",
         message: ""
     });
     const InputEvent = (event) => {
-        const {name, value} = event.target
-        setData((preVal) =>{
-            return{
+        const { name, value } = event.target
+        setData((preVal) => {
+            return {
                 ...preVal,
-                [name]:value,
+                [name]: value,
             }
-        } )
+        })
     }
 
-    const formSubmit = (event) =>{
+    const formSubmit = (event) => {
         event.preventDefault();
-        alert(`
+
+        if (data.fullname === '' || data.phone === '' || data.email === '' || data.subject === '' || data.message === '') {
+            alert(
+                "Please enter valid data to submit form"
+            )
+        }
+
+        else
+            alert(`
             My Name is ${data.fullname}.
             My phone number is ${data.phone}.
             my email Address is ${data.email}.
@@ -30,81 +38,81 @@ const Contact = () => {
         `)
     }
 
-  return (
-    <div>
-        <section className='Contact' id='contact'>
-            <div className='container top'>
-                <div className='heading text-center'>
-                    <h4>Contact Me</h4>
-                    <h1>Get In Touch !</h1>
-                </div>
+    return (
+        <div>
+            <section className='Contact' id='contact'>
+                <div className='container top'>
+                    <div className='heading text-center'>
+                        <h4>Contact Me</h4>
+                        <h1>Get In Touch !</h1>
+                    </div>
 
-                <div className='content d_flex'>
-                    <div className='left'>
-                        <div className='box box_shadow'>
-                            <div className='img'>
-                                <img src='https://tse3.mm.bing.net/th?id=OIP.8sJyH-4awpaExIPf1rGlvAHaDl&pid=Api&P=0' alt='' />
-                            </div>
-                            <div className='details'>
-                                <h1>Vishakha Sahu</h1>
-                                <p>Enjoy connecting to new people and share valuable thoughts </p>
-                            </div>
-                            <span>Let's Catch Up !</span>
-                            <div className='button f-flex'>
-                                <button className='btn_shadow'>
-                                    <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
-                                        <i className='fab fa-facebook-f'></i>
-                                    </a>
-                                </button>
-                                <button className='btn_shadow'>
-                                    <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
-                                        <i className='fab fa-instagram'></i>
-                                    </a>
-                                </button>
-                                <button className='btn_shadow'>
-                                    <a href='https://twitter.com/?lang=en-in' target='_blank' rel='noreferrer'>
-                                        <i className='fab fa-twitter'></i>
-                                    </a>
-                                </button>
+                    <div className='content d_flex'>
+                        <div className='left'>
+                            <div className='box box_shadow'>
+                                <div className='img'>
+                                    <img src='https://tse3.mm.bing.net/th?id=OIP.8sJyH-4awpaExIPf1rGlvAHaDl&pid=Api&P=0' alt='' />
+                                </div>
+                                <div className='details'>
+                                    <h1>Vishakha Sahu</h1>
+                                    <p>Enjoy connecting to new people and share valuable thoughts </p>
+                                </div>
+                                <span>Let's Catch Up !</span>
+                                <div className='button f-flex'>
+                                    <button className='btn_shadow'>
+                                        <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
+                                            <i className='fab fa-facebook-f'></i>
+                                        </a>
+                                    </button>
+                                    <button className='btn_shadow'>
+                                        <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+                                            <i className='fab fa-instagram'></i>
+                                        </a>
+                                    </button>
+                                    <button className='btn_shadow'>
+                                        <a href='https://twitter.com/?lang=en-in' target='_blank' rel='noreferrer'>
+                                            <i className='fab fa-twitter'></i>
+                                        </a>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div className='right box_shadow'>
-                        <form onSubmit={formSubmit}>
-                            <div className=''>
-                                <div className='input row'>
-                                    <span>YOUR NAME</span>
-                                    <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
+                        <div className='right box_shadow'>
+                            <form onSubmit={formSubmit}>
+                                <div className=''>
+                                    <div className='input row'>
+                                        <span>YOUR NAME</span>
+                                        <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
+                                    </div>
+                                    <div className='input row'>
+                                        <span>YOUR NUMBER</span>
+                                        <input type='phone' name='phone' value={data.phone} onChange={InputEvent} />
+                                    </div>
+                                    <div className='input'>
+                                        <span>EMAIL</span>
+                                        <input type='email' name='email' value={data.email} onChange={InputEvent} />
+                                    </div>
+                                    <div className='input'>
+                                        <span>SUBJECT</span>
+                                        <input type='text' name='subject' value={data.subject} onChange={InputEvent} />
+                                    </div>
+                                    <div className='input'>
+                                        <span>YOUR MESSAGE</span>
+                                        <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
+                                    </div>
+                                    <button className='btn_shadow'>
+                                        SEND MESSAGE <i className='fa fa-long-arrow-down'></i>
+                                    </button>
                                 </div>
-                                <div className='input row'>
-                                    <span>YOUR NUMBER</span>
-                                    <input type='phone' name='phone' value={data.phone} onChange={InputEvent} />
-                                </div>
-                                <div className='input'>
-                                    <span>EMAIL</span>
-                                    <input type='email' name='email' value={data.email} onChange={InputEvent} />
-                                </div>
-                                <div className='input'>
-                                    <span>SUBJECT</span>
-                                    <input type='text' name='subject' value={data.subject} onChange={InputEvent} />
-                                </div>
-                                <div className='input'>
-                                    <span>YOUR MESSAGE</span>
-                                    <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
-                                </div>
-                                <button className='btn_shadow'>
-                                    SEND MESSAGE <i className='fa fa-long-arrow-down'></i>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            </form>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        </section>
-    </div>
-  )
+            </section>
+        </div>
+    )
 }
 
 export default Contact;
--- src/Component/Resume/Resume.js
+++ src/Component/Resume/Resume.js
@@ -5,57 +5,57 @@ import './Resume.css';
 
 
 const Resume = () => {
-  return (
-    <>
-        <section className='Resume' id='resume'>
-            <div className='container top'>
-                <div className='heading text-center'>
-                    <h4>1.5 Years of experience in Full Stack Development (Dotnet, React, MySQL)</h4>
-                    <h1>My Resume</h1>
-                </div>
-                <div className='content-section mtop d_flex'>
-                    <div className='left content_edu'>
-                        <div className='heading'>
-                            <h4>2014-2020</h4>
-                            <h1>Education</h1>
-                        </div>
-                        <div className='content '>
-                            {
-                                ResumeApi.map((val, id) => {
-                                    if(val.category === "education"){
-                                        return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
-                                    }
-                                    else{
-                                        return <div></div>
-                                    }
-                                } )
-                            }
-                        </div>
+    return (
+        <>
+            <section className='Resume' id='resume'>
+                <div className='container top'>
+                    <div className='heading text-center'>
+                        <h4>1.5 Years of experience in Full Stack Development (Dotnet, React, MySQL)</h4>
+                        <h1>My Resume</h1>
                     </div>
+                    <div className='content-section mtop d_flex'>
+                        <div className='left content_edu'>
+                            <div className='heading'>
+                                <h4>2014-2020</h4>
+                                <h1>Education</h1>
+                            </div>
+                            <div className='content '>
+                                {
+                                    ResumeApi.map((val, id) => {
+                                        if (val.category === "education") {
+                                            return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                                        }
+                                        else {
+                                            return <div></div>
+                                        }
+                                    })
+                                }
+                            </div>
+                        </div>
 
-                    <div className='left content_exp'>
-                        <div className='heading'>
-                            <h4>2021-2022</h4>
-                            <h1>Job Experience</h1>
-                        </div>
-                        <div className='content '>
-                            {
-                                ResumeApi.map((val, id) => {
-                                    if(val.category === "experience"){
-                                        return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
-                                    }
-                                    else{
-                                        return <div></div>
-                                    }
-                                } )
-                            }
+                        <div className='left content_exp'>
+                            <div className='heading'>
+                                <h4>2021-2022</h4>
+                                <h1>Job Experience</h1>
+                            </div>
+                            <div className='content '>
+                                {
+                                    ResumeApi.map((val, id) => {
+                                        if (val.category === "experience") {
+                                            return <Card key={id} title={val.title} year={val.year} rate={val.rate} desc={val.desc} />
+                                        }
+                                        else {
+                                            return <div></div>
+                                        }
+                                    })
+                                }
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    </>
-  )
+            </section>
+        </>
+    )
 }
 
 export default Resume;

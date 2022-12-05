a15787e76d28359dc0c4b8e130c9d87bcb4bea77
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-12-05 14:34:25 +0530

    v - 20 profile picture removed

--- src/Component/Head/Header.js
+++ src/Component/Head/Header.js
@@ -4,42 +4,42 @@ import logo from '../pic/logo.jpg';
 
 const Header = () => {
   //When scroll header at top
-  window.addEventListener("scroll", function(){
+  window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header")
-    header.classList.toggle("active", this.window.scrollY > 100 )
-  } )
+    header.classList.toggle("active", this.window.scrollY > 100)
+  })
 
   // Toggle menu
   const [Mobile, setMobile] = useState(false);
 
   return (
     <>
-        <header className='header'>
-            <div className='container d_flex'>
-              <div className='logo'>
-                <img src={logo} className='logo_main' alt='logo Vishakha' height="60px" /> 
-              </div>
-              <div className='navlink'>
-                  <ul className={Mobile ? "nav-links-mobile": "link f_flex uppercase" } onClick={() => setMobile(false)} >
-                  <li><a href='#home'>Home</a> </li>
-                  {/* <li><a href='#features'>Features</a> </li> */}
-                  <li><a href='#portfolio'>Portfolio</a> </li>
-                  <li><a href='#resume'>Resume</a> </li>
-                  <li><a href='#blog'>Book-a-Fact</a> </li>
-                  {/* <li><a href='#blog'>Book-a-Fact</a> </li> */}
-                  <li><a href='#contact'>Contact</a> </li>
-                  {/* <li><button className='homne-btn'>BUY NOW</button> </li> */}
-                </ul>
-                <button className='toggle' onClick={() => setMobile(!Mobile)} >
-                  {
-                    Mobile ? 
-                      <i class="fa fa-times close home-btn"></i> :
-                      <i class="fa fa-bars open"></i>
-                  }
-                </button>
-              </div>
-            </div>
-        </header>
+      <header className='header'>
+        <div className='container d_flex'>
+          <div className='logo'>
+            <img src={logo} className='logo_main' alt='logo Vishakha' height="60px" />
+          </div>
+          <div className='navlink'>
+            <ul className={Mobile ? "nav-links-mobile" : "link f_flex uppercase"} onClick={() => setMobile(false)} >
+              <li><a href='#home'>Home</a> </li>
+              {/* <li><a href='#features'>Features</a> </li> */}
+              <li><a href='#portfolio'>Portfolio</a> </li>
+              <li><a href='#resume'>Resume</a> </li>
+              <li><a href='#blog'>Book-a-Fact</a> </li>
+              {/* <li><a href='#blog'>Book-a-Fact</a> </li> */}
+              <li><a href='#contact'>Contact</a> </li>
+              {/* <li><button className='homne-btn'>BUY NOW</button> </li> */}
+            </ul>
+            <button className='toggle' onClick={() => setMobile(!Mobile)} >
+              {
+                Mobile ?
+                  <i class="fa fa-times close home-btn"></i> :
+                  <i class="fa fa-bars open"></i>
+              }
+            </button>
+          </div>
+        </div>
+      </header>
     </>
   )
 }
--- src/Component/HomeH/Home.js
+++ src/Component/HomeH/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import {Typewriter} from 'react-simple-typewriter';
+import { Typewriter } from 'react-simple-typewriter';
 import profileP from '../pic/profilephoto.png';
 
 const Home = () => {
@@ -9,12 +9,12 @@ const Home = () => {
       <section className="hero" id="home">
         <div className="container f_flex top">
           <div className="left top">
-            <h3>Welcome to my World!!</h3>
+            <h3>Welcome to my Website!</h3>
             <h1>
               Hi I'm <span>Vishakha Sahu</span>
             </h1>
             <h2>
-                a
+              a
               <Typewriter
                 words={[" Full Stack Dev", " React Developer", " Web Developer"]}
                 loop
@@ -27,43 +27,43 @@ const Home = () => {
             </h2>
 
             <div className='hero_btn d_flex'>
-                <div className='col_1'>
-                    <h4>FIND ME ON</h4>
-                    <div className='button'>
-                        <button className='btn_shadow'>
-                          <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
-                            <i className='fab fa-facebook-f'></i>
-                          </a>
-                        </button>
-                        <button className='btn_shadow'>
-                          <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
-                          <i className='fab fa-instagram'></i>
-                          </a>
-                        </button>
-                        <button className='btn_shadow'>
-                          <a href='https://www.linkedin.com/in/vishakhasahu/' target='_blank' rel='noreferrer'>
-                            <i className='fab fa-linkedin-in'></i>
-                          </a>
-                        </button>
-                    </div>
+              <div className='col_1'>
+                <h4>FIND ME ON</h4>
+                <div className='button'>
+                  <button className='btn_shadow'>
+                    <a href='https://www.facebook.com/' target='_blank' rel='noreferrer'>
+                      <i className='fab fa-facebook-f'></i>
+                    </a>
+                  </button>
+                  <button className='btn_shadow'>
+                    <a href='https://www.instagram.com/' target='_blank' rel='noreferrer'>
+                      <i className='fab fa-instagram'></i>
+                    </a>
+                  </button>
+                  <button className='btn_shadow'>
+                    <a href='https://www.linkedin.com/in/vishakhasahu/' target='_blank' rel='noreferrer'>
+                      <i className='fab fa-linkedin-in'></i>
+                    </a>
+                  </button>
                 </div>
+              </div>
 
-                <div className='col_1'>
-                    <h4>BEST SKILL ON</h4>
-                    <button className='btn_shadow'>
-                      <a href='https://github.com/vish23698sahu' target='_blank' rel='noreferrer'>
-                        <i class="fa-brands fa-github"></i>
-                      </a>
-                    </button>
-                </div>
+              <div className='col_1'>
+                <h4>BEST SKILL ON</h4>
+                <button className='btn_shadow'>
+                  <a href='https://github.com/vish23698sahu' target='_blank' rel='noreferrer'>
+                    <i class="fa-brands fa-github"></i>
+                  </a>
+                </button>
+              </div>
             </div>
           </div>
 
-          <div className='right'>
+          {/* <div className='right'>
             <div className='right_img'>
                 <img src={profileP} height='650px' alt='profile' />
             </div>
-          </div>
+          </div> */}
 
         </div>
       </section>
